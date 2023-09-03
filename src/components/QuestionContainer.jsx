0b1434abab0da391115b5d2bdebff2f4a806f074
Dzabol <sebastian.jablecki@gmail.com>
2023-09-03 15:00:24 +0200

    Corrected id

--- src/components/QuestionContainer.jsx
+++ src/components/QuestionContainer.jsx
@@ -1,17 +1,22 @@
+import React from "react";
+
 export default function QuestionContainer(props) {
-  const id = crypto.randomUUID();
-  const answers = props.question.answers.map((answer) => (
-    <div key={crypto.randomUUID()}>
-      <input
-        type="radio"
-        id={crypto.randomUUID()}
-        name={id}
-        value={answer.answer}
-        onChange={props.handleChange}
-      />
-      <label htmlFor={crypto.randomUUID()}>{answer.answer}</label>
-    </div>
-  ));
+  const answers_id = crypto.randomUUID();
+  const answers = props.question.answers.map((answer) => {
+    const answer_id = crypto.randomUUID();
+    return (
+      <div key={crypto.randomUUID()}>
+        <input
+          type="radio"
+          id={answer_id}
+          name={answers_id}
+          value={answer.answer}
+          onChange={props.handleChange}
+        />
+        <label htmlFor={answer_id}>{answer.answer}</label>
+      </div>
+    );
+  });
 
   return (
     <div>
