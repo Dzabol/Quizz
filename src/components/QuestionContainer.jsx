aab6200b1f9d70970486453c57e8c4af206c697e
Dzabol <sebastian.jablecki@gmail.com>
2023-09-10 23:16:51 +0200

    Added visual aspects ok/nok

--- src/components/QuestionContainer.jsx
+++ src/components/QuestionContainer.jsx
@@ -2,6 +2,24 @@ import React from "react";
 
 export default function QuestionContainer(props) {
   const answers = props.question.answers.map((answer) => {
+    const isChecked = props.allQuestions.some((question) => {
+      return (
+        question.questionID === props.id &&
+        question.userAnswer === answer.answer
+      );
+    });
+
+    const answerBackGroundColor = () => {
+      if (isChecked && answer.isCorrect) {
+        return " correctAnswer";
+      }
+      if (isChecked && !answer.isCorrect) {
+        return " wrongAnswer";
+      }
+      if (!isChecked && answer.isCorrect) {
+        return " correctAnswer";
+      } else return "";
+    };
     return (
       <div key={crypto.randomUUID()} className="radio-container">
         <input
@@ -11,15 +29,15 @@ export default function QuestionContainer(props) {
           value={answer.answer}
           data-is-answer-correct={answer.isCorrect}
           onChange={props.onChange}
-          checked={props.allQuestions.some((question) => {
-            return (
-              question.questionID === props.id &&
-              question.userAnswer === answer.answer
-            );
-          })}
+          checked={isChecked}
           disabled={!props.disabled}
         />
-        <label htmlFor={answer.answerID} className="radio-label">
+        <label
+          htmlFor={answer.answerID}
+          className={
+            "radio-label" + (!props.disabled ? answerBackGroundColor() : "")
+          }
+        >
           {answer.answer}
         </label>
       </div>
